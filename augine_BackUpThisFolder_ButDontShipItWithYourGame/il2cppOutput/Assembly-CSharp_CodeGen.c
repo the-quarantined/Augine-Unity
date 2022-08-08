4d744af9c07ef3936f0c68762aad338b848e0577
kavyarishi19 <forcodingpurposes19@gmail.com>
2022-08-08 13:32:44 +0530

    Final Updates For HackOn

---
+++ augine_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,145 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ButtonManager::Start()
+extern void ButtonManager_Start_m1031E938A2FB8D85F993AA7DF29D06DAF0CDF930 (void);
+// 0x00000002 System.Void ButtonManager::Update()
+extern void ButtonManager_Update_m8A6A02F642A23DDDD0D1C1B1EB563E144AE12590 (void);
+// 0x00000003 System.Void ButtonManager::SelectObject()
+extern void ButtonManager_SelectObject_mF93C06A3BF39977EF595DAF7620219F3F96FD153 (void);
+// 0x00000004 System.Void ButtonManager::.ctor()
+extern void ButtonManager__ctor_m7DBD91D2AF27494F6AC7DC74DE679919AB1DD71F (void);
+// 0x00000005 DataHandler DataHandler::get_Instance()
+extern void DataHandler_get_Instance_m9FCB9BE5B72BE8E6D3594F83D3E6040876CB5530 (void);
+// 0x00000006 System.Void DataHandler::.ctor()
+extern void DataHandler__ctor_m012F95496F34E79A92969822FE849DAC5FDFA7C1 (void);
+// 0x00000007 System.Void InputManager::Start()
+extern void InputManager_Start_m1C1F4D73A508DB32B5D6E294F12240923279FB10 (void);
+// 0x00000008 System.Void InputManager::Update()
+extern void InputManager_Update_m5CEF4B04E7336E0F24B004F98C71799F3DFD5656 (void);
+// 0x00000009 System.Boolean InputManager::IsPointerOverUI(UnityEngine.Touch)
+extern void InputManager_IsPointerOverUI_m2C11C48548EFF484F1954E40059BF9D5C8D34A15 (void);
+// 0x0000000A System.Void InputManager::CrosshairCalculation()
+extern void InputManager_CrosshairCalculation_m26C86F90D23013848751B2E915C1138D22FA8E43 (void);
+// 0x0000000B System.Void InputManager::.ctor()
+extern void InputManager__ctor_m52D2F3B9FA0D50C52BCC92486F49B300E9334C2A (void);
+// 0x0000000C System.Void LineManager::Start()
+extern void LineManager_Start_m0C9EDAADA590E07337536D75CDC7C3A821143A28 (void);
+// 0x0000000D System.Void LineManager::DrawLine(UnityEngine.XR.Interaction.Toolkit.AR.ARObjectPlacementEventArgs)
+extern void LineManager_DrawLine_mA27CC583A00DAF89B63E2BF4C0C18D6119CAF982 (void);
+// 0x0000000E System.Void LineManager::.ctor()
+extern void LineManager__ctor_m3F224326E219E1C7F4AD715826F863BE8C3E0221 (void);
+// 0x0000000F System.Void PlaneMatManager::Awake()
+extern void PlaneMatManager_Awake_m991C10CE800255067367094298165344F19938E1 (void);
+// 0x00000010 System.Void PlaneMatManager::Update()
+extern void PlaneMatManager_Update_m2787D6C98E1EAE598B60323DCFA2D1D80CBF06B7 (void);
+// 0x00000011 System.Void PlaneMatManager::OnClickButton(UnityEngine.Texture)
+extern void PlaneMatManager_OnClickButton_m0B8D78CF69ECC15A7A1FC1A9B6D61CB958DEECB4 (void);
+// 0x00000012 System.Void PlaneMatManager::.ctor()
+extern void PlaneMatManager__ctor_m32D749FFEC2DDA72C5E4021BFDB623335388926F (void);
+// 0x00000013 System.Void PlaneMatManager/<>c__DisplayClass2_0::.ctor()
+extern void U3CU3Ec__DisplayClass2_0__ctor_m2AD215C5149446A519C6745D39CEAA05025A0AB4 (void);
+// 0x00000014 System.Void PlaneMatManager/<>c__DisplayClass2_0::<Awake>b__0()
+extern void U3CU3Ec__DisplayClass2_0_U3CAwakeU3Eb__0_m66D00655BEA1DB494DCA095E8182E2FD288C66CA (void);
+// 0x00000015 System.Void ScreenManager::playARTiles()
+extern void ScreenManager_playARTiles_m47CFE2C662477C9F2398B291CFB6B66DD4554A9E (void);
+// 0x00000016 System.Void ScreenManager::playARMeasure()
+extern void ScreenManager_playARMeasure_mAE9861A6991AA3EFCB1F496CF13FB11EE5E65C9F (void);
+// 0x00000017 System.Void ScreenManager::playARFurniture()
+extern void ScreenManager_playARFurniture_m300CCDA7DA16102D5C2F2684B616F491B882F27A (void);
+// 0x00000018 System.Void ScreenManager::BackARFurniture()
+extern void ScreenManager_BackARFurniture_m690DFA34BFF5E937C245F1A8D1D840B04F9F66B4 (void);
+// 0x00000019 System.Void ScreenManager::BackARTiles()
+extern void ScreenManager_BackARTiles_m1F06E9C6672DF59E8523C4F71764F3FB2E53123F (void);
+// 0x0000001A System.Void ScreenManager::.ctor()
+extern void ScreenManager__ctor_m6F499D29D3592ED6030368C9EB312A26D1110E1E (void);
+static Il2CppMethodPointer s_methodPointers[26] = 
+{
+	ButtonManager_Start_m1031E938A2FB8D85F993AA7DF29D06DAF0CDF930,
+	ButtonManager_Update_m8A6A02F642A23DDDD0D1C1B1EB563E144AE12590,
+	ButtonManager_SelectObject_mF93C06A3BF39977EF595DAF7620219F3F96FD153,
+	ButtonManager__ctor_m7DBD91D2AF27494F6AC7DC74DE679919AB1DD71F,
+	DataHandler_get_Instance_m9FCB9BE5B72BE8E6D3594F83D3E6040876CB5530,
+	DataHandler__ctor_m012F95496F34E79A92969822FE849DAC5FDFA7C1,
+	InputManager_Start_m1C1F4D73A508DB32B5D6E294F12240923279FB10,
+	InputManager_Update_m5CEF4B04E7336E0F24B004F98C71799F3DFD5656,
+	InputManager_IsPointerOverUI_m2C11C48548EFF484F1954E40059BF9D5C8D34A15,
+	InputManager_CrosshairCalculation_m26C86F90D23013848751B2E915C1138D22FA8E43,
+	InputManager__ctor_m52D2F3B9FA0D50C52BCC92486F49B300E9334C2A,
+	LineManager_Start_m0C9EDAADA590E07337536D75CDC7C3A821143A28,
+	LineManager_DrawLine_mA27CC583A00DAF89B63E2BF4C0C18D6119CAF982,
+	LineManager__ctor_m3F224326E219E1C7F4AD715826F863BE8C3E0221,
+	PlaneMatManager_Awake_m991C10CE800255067367094298165344F19938E1,
+	PlaneMatManager_Update_m2787D6C98E1EAE598B60323DCFA2D1D80CBF06B7,
+	PlaneMatManager_OnClickButton_m0B8D78CF69ECC15A7A1FC1A9B6D61CB958DEECB4,
+	PlaneMatManager__ctor_m32D749FFEC2DDA72C5E4021BFDB623335388926F,
+	U3CU3Ec__DisplayClass2_0__ctor_m2AD215C5149446A519C6745D39CEAA05025A0AB4,
+	U3CU3Ec__DisplayClass2_0_U3CAwakeU3Eb__0_m66D00655BEA1DB494DCA095E8182E2FD288C66CA,
+	ScreenManager_playARTiles_m47CFE2C662477C9F2398B291CFB6B66DD4554A9E,
+	ScreenManager_playARMeasure_mAE9861A6991AA3EFCB1F496CF13FB11EE5E65C9F,
+	ScreenManager_playARFurniture_m300CCDA7DA16102D5C2F2684B616F491B882F27A,
+	ScreenManager_BackARFurniture_m690DFA34BFF5E937C245F1A8D1D840B04F9F66B4,
+	ScreenManager_BackARTiles_m1F06E9C6672DF59E8523C4F71764F3FB2E53123F,
+	ScreenManager__ctor_m6F499D29D3592ED6030368C9EB312A26D1110E1E,
+};
+static const int32_t s_InvokerIndices[26] = 
+{
+	5653,
+	5653,
+	5653,
+	5653,
+	8822,
+	5653,
+	5653,
+	5653,
+	3176,
+	5653,
+	5653,
+	5653,
+	4464,
+	5653,
+	5653,
+	5653,
+	4464,
+	5653,
+	5653,
+	5653,
+	5653,
+	5653,
+	5653,
+	5653,
+	5653,
+	5653,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	26,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
